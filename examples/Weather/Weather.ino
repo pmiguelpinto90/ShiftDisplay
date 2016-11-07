b905c6ecd6bf7c184197b5a6deed2a1bdc3d35bf
Miguel Pynto <miguel@pynto.me>
2016-11-07 23:49:52 +0000

    Weather example as common anode

--- examples/Weather/Weather.ino
+++ examples/Weather/Weather.ino
@@ -3,7 +3,7 @@
 const int LATCH_PIN = 6;
 const int CLOCK_PIN = 7;
 const int DATA_PIN = 5;
-const int DISPLAY_TYPE = COMMON_CATHODE;
+const int DISPLAY_TYPE = COMMON_ANODE;
 const int DISPLAY_SIZE = 8; // number of digits
 
 ShiftDisplay disp(LATCH_PIN, CLOCK_PIN, DATA_PIN, DISPLAY_TYPE, DISPLAY_SIZE);

5a6e827dec3545cf59657cb65a82a46b538eec30
Miguel Pynto <miguel@pynto.me>
2016-11-07 23:48:56 +0000

    Weather example bugfix

--- examples/Weather/Weather.ino
+++ examples/Weather/Weather.ino
@@ -1,6 +1,5 @@
 #include <ShiftDisplay.h>
 
-
 const int LATCH_PIN = 6;
 const int CLOCK_PIN = 7;
 const int DATA_PIN = 5;
@@ -9,27 +8,23 @@ const int DISPLAY_SIZE = 8; // number of digits
 
 ShiftDisplay disp(LATCH_PIN, CLOCK_PIN, DATA_PIN, DISPLAY_TYPE, DISPLAY_SIZE);
 
-
 float getTemperature() {
 	delay(1000);
 	return 21.36;
 }
-
 
 String getCondition() {
 	delay(1000);
 	return "Sunny";
 }
 
-
 void setup() {
 }
-
 
 void loop() {
 	float temp = getTemperature();
 	String condition = getCondition();
-	condition = = "        " + condition;
+	condition = "        " + condition;
 
 	// print temperature with one decimal number, aligned to center of display, for 4 seconds
 	disp.print(4000, temp, 1, ALIGN_CENTER);

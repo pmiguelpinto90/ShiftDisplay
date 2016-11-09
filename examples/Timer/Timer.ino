d010e6ad56e40fd00a30fc9c364b4f7575329405
Miguel Pynto <miguel@pynto.me>
2016-11-09 01:21:04 +0000

    Timer example finished

--- examples/Timer/Timer.ino
+++ examples/Timer/Timer.ino
@@ -5,16 +5,48 @@ const int CLOCK_PIN = 7;
 const int DATA_PIN = 5;
 const int DISPLAY_TYPE = COMMON_CATHODE;
 const int DISPLAY_SIZE = 3; // number of digits
-//const int BUTTON_PIN = 2;
+const int BUTTON_PIN = 2;
+
+volatile bool buttonPressed;
 
 ShiftDisplay disp(LATCH_PIN, CLOCK_PIN, DATA_PIN, DISPLAY_TYPE, DISPLAY_SIZE);
 
+void debounce() {
+	disp.show(100); // execution will delay for 100ms
+}
+
+void buttonPressInterrupt() {
+	buttonPressed = true;
+}
+
 void setup() {
-	for (int i = 600; i > 0; i--)
-		disp.print(100, i);
-	disp.set(0);
+	pinMode(BUTTON_PIN, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonPressInterrupt, FALLING);
+	buttonPressed = false;
 }
 
 void loop() {
-	disp.show();
+	float n;
+	for (float i = 600; i > 0; i--) {
+		n = i / 10;
+		if (buttonPressed) {
+
+			// pause
+			debounce();
+			buttonPressed = false;
+			while (!buttonPressed)
+				disp.show(); // show previous value (that was on display when button was pressed)
+
+			// unpause
+			debounce();
+			buttonPressed = false;
+		}
+		disp.print(97, n, 1); // 97 instead of 100 for computing compensation, float with one decimal place
+	}
+
+	disp.set("end");
+	while (!buttonPressed)
+		disp.show();
+	debounce();
+	buttonPressed = false;
 }

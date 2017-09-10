ec12110f29d5b7b3cdefbfa9508e2058eae9e980
MiguelPynto <miguelpynto@outlook.com>
2017-09-10 02:38:06 +0100

    Modified code squeleton and comments

--- src/ShiftDisplay.cpp
+++ src/ShiftDisplay.cpp
@@ -9,33 +9,24 @@ https://miguelpynto.github.io/ShiftDisplay/
 #include "ShiftDisplay.h"
 #include "CharacterEncoding.h"
 
-// *****************************************************************************
-// CONSTRUCTORS
+// CONSTRUCTORS ****************************************************************
 
-// Initializes the library with the single display information and use default pin numbers
 ShiftDisplay::ShiftDisplay(int displayType, int displaySize) {
 	constructSingle(DEFAULT_LATCH_PIN, DEFAULT_CLOCK_PIN, DEFAULT_DATA_PIN, displayType, displaySize);
 }
 
-// Initializes the library with the single display information and pin numbers
 ShiftDisplay::ShiftDisplay(int latchPin, int clockPin, int dataPin, int displayType, int displaySize) {
 	constructSingle(latchPin, clockPin, dataPin, displayType, displaySize);
 }
 
-// Initializes the library with the multiple displays information and use default pin numbers
 ShiftDisplay::ShiftDisplay(int displayType, int displayQuantity, int displaySizes[]) {
 	constructMultiple(DEFAULT_LATCH_PIN, DEFAULT_CLOCK_PIN, DEFAULT_DATA_PIN, displayType, displayQuantity, displaySizes);
 }
 
-// Initializes the library with the multiple displays information and pin numbers
 ShiftDisplay::ShiftDisplay(int latchPin, int clockPin, int dataPin, int displayType, int displayQuantity, int displaySizes[]) {
 	constructMultiple(latchPin, clockPin, dataPin, displayType, displayQuantity, displaySizes);
 }
 
-// *****************************************************************************
-// PRIVATE FUNCTIONS
-
-// Function with common instructions to be called by single display constructors
 void ShiftDisplay::constructSingle(int latchPin, int clockPin, int dataPin, int displayType, int displaySize) {
 	pinMode(latchPin, OUTPUT);
 	pinMode(clockPin, OUTPUT);
@@ -55,7 +46,6 @@ void ShiftDisplay::constructSingle(int latchPin, int clockPin, int dataPin, int 
 	memset(_buffer, initial, MAX_DISPLAY_SIZE); // fill buffer with initial character
 }
 
-// Function with common instructions to be called by multiple display constructors
 void ShiftDisplay::constructMultiple(int latchPin, int clockPin, int dataPin, int displayType, int displayQuantity, int displaySizes[]) {
 	pinMode(latchPin, OUTPUT);
 	pinMode(clockPin, OUTPUT);
@@ -86,47 +76,67 @@ void ShiftDisplay::constructMultiple(int latchPin, int clockPin, int dataPin, in
 	memset(_buffer, initial, MAX_DISPLAY_SIZE); // fill buffer with initial character
 }
 
-// Calculate the length of an array of chars for an integer number
-int ShiftDisplay::countCharacters(long number) {
-	if (number < 0) // negative number
-		return 1 + countCharacters(-number); // minus counts as a character
-	if (number < 10)
-		return 1;
-	return 1 + countCharacters(number / 10);
+// PRIVATE FUNCTIONS ***********************************************************
+
+void ShiftDisplay::showDisplay() {
+	for (int i = 0; i < _displaySize; i++) {
+		digitalWrite(_latchPin, LOW);
+
+		// data for last shift register
+		byte out = _displayType ? ~INDEXES[i] : INDEXES[i];
+		shiftOut(_dataPin, _clockPin, LSBFIRST, out);
+
+		// data for first shift register
+		shiftOut(_dataPin, _clockPin, LSBFIRST, _buffer[i]);
+
+		digitalWrite(_latchPin, HIGH);
+
+		delay(POV);
+	}
 }
 
-// Calculate the length of an array of chars for the integer part on a real number
-int ShiftDisplay::countCharacters(double number) {
-	if (number > -1 && number < 0) // -0.x
-		return 2; // minus and zero count as 2 characters
-	return countCharacters((long) number);
+void ShiftDisplay::clearDisplay() {
+	digitalWrite(_latchPin, LOW);
+	shiftOut(_dataPin, _clockPin, MSBFIRST, 0); // both ends of led with same value
+	shiftOut(_dataPin, _clockPin, MSBFIRST, 0);
+	digitalWrite(_latchPin, HIGH);
 }
 
-// Convert an integer number to an array of characters
-void ShiftDisplay::getCharacters(long input, int size, char output[]) {
+void ShiftDisplay::setBuffer(int start, int size, byte input[]) {
+	for (int i = 0; i < size; i++)
+		_buffer[i+start] = _displayType ? input[i] : ~input[i];
+}
 
-	// invert negative
-	bool negative = false;
-	if (input < 0) {
-		negative = true;
-		input = -input;
+void ShiftDisplay::setBufferDot(int index, bool show) {
+	int bit;
+	if (show)
+		bit = _displayType ? 1 : 0;
+	else
+		bit = _displayType ? 0 : 1;
+	//int bit = (show == (bool)_displayType) TODO
+	bitWrite(_buffer[index], 0, bit);
+}
+
+void ShiftDisplay::encodeCharacters(int size, const char input[], byte output[], int dotIndex = NULL) {
+	for (int i = 0; i < size; i++) {
+		char c = input[i];
+		
+		if (c >= '0' && c <= '9')
+			output[i] = NUMBERS[c - '0'];
+		else if (c >= 'a' && c <= 'z')
+			output[i] = LETTERS[c - 'a'];
+		else if (c >= 'A' && c <= 'Z')
+			output[i] = LETTERS[c - 'A'];
+		else if (c == '-')
+			output[i] = MINUS;
+		else // space or invalid
+			output[i] = BLANK;
 	}
 
-	// iterate every array position, even if all zeros
-	for (int i = size-1; i >= 0; i--) {
-		int digit = input % 10;
-		char c = digit + '0';
-		output[i] = c;
-		input /= 10;
-	}
-
-	// insert a minus character if negative
-	if (negative)
-		output[0] = '-';
+	if (dotIndex != NULL)
+		bitWrite(output[dotIndex], 0, 1);
 }
 
-// Arrange characters for display format over specified alignment
-// Return dot index on display or NULL if none
 int ShiftDisplay::formatCharacters(int inSize, const char input[], int outSize, char output[], char alignment, int decimalPlaces = NULL) {
 	
 	// index of character virtual borders
@@ -166,79 +176,48 @@ int ShiftDisplay::formatCharacters(int inSize, const char input[], int outSize, 
 	return dotIndex;
 }
 
-// Encode array of characters to array of bytes read by the display
-void ShiftDisplay::encodeCharacters(int size, const char input[], byte output[], int dotIndex = NULL) {
-	for (int i = 0; i < size; i++) {
-		char c = input[i];
-		
-		if (c >= '0' && c <= '9')
-			output[i] = NUMBERS[c - '0'];
-		else if (c >= 'a' && c <= 'z')
-			output[i] = LETTERS[c - 'a'];
-		else if (c >= 'A' && c <= 'Z')
-			output[i] = LETTERS[c - 'A'];
-		else if (c == '-')
-			output[i] = MINUS;
-		else // space or invalid
-			output[i] = BLANK;
+void ShiftDisplay::getCharacters(long input, int size, char output[]) {
+
+	// invert negative
+	bool negative = false;
+	if (input < 0) {
+		negative = true;
+		input = -input;
 	}
 
-	if (dotIndex != NULL)
-		bitWrite(output[dotIndex], 0, 1);
-}
-
-// Modify buffer, add or remove a dot at index
-void ShiftDisplay::setBufferDot(int index, bool show) {
-	int bit;
-	if (show)
-		bit = _displayType ? 1 : 0;
-	else
-		bit = _displayType ? 0 : 1;
-	//int bit = (show == (bool)_displayType) TODO
-	bitWrite(_buffer[index], 0, bit);
-}
-
-// Save data to buffer, ready to be read by display
-void ShiftDisplay::setBuffer(int start, int size, byte input[]) {
-	for (int i = 0; i < size; i++)
-		_buffer[i+start] = _displayType ? input[i] : ~input[i];
-}
-
-// Clear display
-void ShiftDisplay::clearDisplay() {
-	digitalWrite(_latchPin, LOW);
-	shiftOut(_dataPin, _clockPin, MSBFIRST, 0); // both ends of led with same value
-	shiftOut(_dataPin, _clockPin, MSBFIRST, 0);
-	digitalWrite(_latchPin, HIGH);
-}
-
-// Iterate buffer value on display, achieving persistence of vision
-void ShiftDisplay::printDisplay() {
-	for (int i = 0; i < _displaySize; i++) {
-		digitalWrite(_latchPin, LOW);
-
-		// data for last shift register
-		byte out = _displayType ? ~INDEXES[i] : INDEXES[i];
-		shiftOut(_dataPin, _clockPin, LSBFIRST, out);
-
-		// data for first shift register
-		shiftOut(_dataPin, _clockPin, LSBFIRST, _buffer[i]);
-
-		digitalWrite(_latchPin, HIGH);
-
-		delay(1); // milliseconds showing a single character
+	// iterate every array position, even if all zeros
+	for (int i = size-1; i >= 0; i--) {
+		int digit = input % 10;
+		char c = digit + '0';
+		output[i] = c;
+		input /= 10;
 	}
+
+	// insert a minus character if negative
+	if (negative)
+		output[0] = '-';
 }
 
-// *****************************************************************************
-// PUBLIC FUNCTIONS
+int ShiftDisplay::countCharacters(long number) {
+	if (number < 0) // negative number
+		return 1 + countCharacters(-number); // minus counts as a character
+	if (number < 10)
+		return 1;
+	return 1 + countCharacters(number / 10);
+}
 
-// Save to buffer an int value
+int ShiftDisplay::countCharacters(double number) {
+	if (number > -1 && number < 0) // -0.x
+		return 2; // minus and zero count as 2 characters
+	return countCharacters((long) number);
+}
+
+// PUBLIC FUNCTIONS ************************************************************
+
 void ShiftDisplay::set(int value, char alignment) {
 	set((long) value, alignment); // call long function
 }
 
-// Save to buffer a long value
 void ShiftDisplay::set(long value, char alignment) {
 	int size = countCharacters(value);
 	char originalCharacters[size];
@@ -250,7 +229,6 @@ void ShiftDisplay::set(long value, char alignment) {
 	setBuffer(0, _displaySize, encodedCharacters);
 }
 
-// Save to buffer a double value
 void ShiftDisplay::set(double value, int decimalPlaces, char alignment) {
 
 	// if no decimal places, call integer function instead
@@ -273,12 +251,10 @@ void ShiftDisplay::set(double value, int decimalPlaces, char alignment) {
 	setBuffer(0, _displaySize, encodedCharacters);
 }
 
-// Save to buffer a double value with default number of decimal places
 void ShiftDisplay::set(double value, char alignment) {
 	set(value, DEFAULT_DECIMAL_PLACES, alignment);
 }
 
-// Save to buffer a char value
 void ShiftDisplay::set(char value, char alignment) {
 	char originalCharacters[] = {value};
 	char formattedCharacters[_displaySize];
@@ -288,7 +264,6 @@ void ShiftDisplay::set(char value, char alignment) {
 	setBuffer(0, _displaySize, encodedCharacters);
 }
 
-// Save to buffer a char array value
 void ShiftDisplay::set(const char value[], char alignment) {
 	int size = strlen(value);
 	char formattedCharacters[_displaySize];
@@ -298,7 +273,6 @@ void ShiftDisplay::set(const char value[], char alignment) {
 	setBuffer(0, _displaySize, encodedCharacters);
 }
 
-// Save to buffer an Arduino String value, manual processing for better support between Arduino cores
 void ShiftDisplay::set(const String &value, char alignment) {
 
 	// get String size
@@ -315,12 +289,10 @@ void ShiftDisplay::set(const String &value, char alignment) {
 	set(str, alignment); // call char array function
 }
 
-// Save to buffer a formatted segments array
 void ShiftDisplay::set(const byte codes[]) {
 	setBuffer(0, _displaySize, codes);
 }
 
-// Save to buffer a formatted characters array and a dots array
 void ShiftDisplay::set(const char characters[], bool dots[]) {
 	byte encodedCharacters[_displaySize];
 	encodeCharacters(_displaySize, characters, encodedCharacters);
@@ -330,12 +302,10 @@ void ShiftDisplay::set(const char characters[], bool dots[]) {
 			setBufferDot(i, dots[i]);
 }
 
-// Save to a section of buffer an int value
 void ShiftDisplay::setAt(int displayId, int value, char alignment) {
 	setAt(displayId, (long) value, alignment); // call long function
 }
 
-// Save to a section of buffer a long value
 void ShiftDisplay::setAt(int displayId, long value, char alignment) {
 	if (displayId >= 0 && displayId < _displayQuantity) { // valid displayId
 		int valueSize = countCharacters(value);
@@ -350,122 +320,102 @@ void ShiftDisplay::setAt(int displayId, long value, char alignment) {
 	}
 }
 
-//
 void ShiftDisplay::setAt(int displayId, double value, int decimalPlaces, char alignment) {
-
+	// TODO
 }
 
-//
 void ShiftDisplay::setAt(int displayId, double value, char alignment) {
-
+	// TODO
 }
 
-//
 void ShiftDisplay::setAt(int displayId, char value, char alignment) {
-
+	// TODO
 }
 
-//
 void ShiftDisplay::setAt(int displayId, const char value[], char alignment) {
-
+	// TODO
 }
 
-//
 void ShiftDisplay::setAt(int displayId, const String &value, char alignment) {
-
+	// TODO
 }
 
-//
 void ShiftDisplay::setAt(int displayId, const byte codes[]) {
-
+	// TODO
 }
 
-//
 void ShiftDisplay::setAt(int displayId, const char characters[], bool dots[]) {
-
+	// TODO
 }
 
-// Modify buffer, insert dot at index
 void ShiftDisplay::insertDot(int index) {
 	if (index >= 0 && index < _displaySize)
 		setBufferDot(index, true);
 }
 
-// Modify buffer, remove dot at index
 void ShiftDisplay::removeDot(int index) {
 	if (index >= 0 && index < _displaySize)
 		setBufferDot(index, false);
 }
 
-// Show buffer value for one iteration
 void ShiftDisplay::show() {
-	printDisplay();
+	showDisplay();
 	clearDisplay();
 }
 
-// Show buffer value for the specified time
 void ShiftDisplay::show(unsigned long time) {
-	unsigned long end = millis() + time - (1*_displaySize); // start + total duration - last iteration (so it doesnt exceed time requested)
+	unsigned long end = millis() + time - (POV*_displaySize); // start + total duration - last iteration (so it doesnt exceed time requested)
 	while (millis() <= end)
-		printDisplay();
+		showDisplay();
 	clearDisplay();
 }
 
-// Save to buffer and show an int value for the specified time
 void ShiftDisplay::show(int value, unsigned long time, char alignment) {
 	set(value, alignment);
 	show(time);
 }
 
-// Save to buffer and show a long value for the specified time
 void ShiftDisplay::show(long value, unsigned long time, char alignment) {
 	set(value, alignment);
 	show(time);
 }
 
-// Save to buffer and show a double value for the specified time
 void ShiftDisplay::show(double value, unsigned long time, int decimalPlaces, char alignment) {
 	set(value, decimalPlaces, alignment);
 	show(time);
 }
 
-// Save to buffer and show a double value with default number of decimal places for the specified time
 void ShiftDisplay::show(double value, unsigned long time, char alignment) {
 	set(value, DEFAULT_DECIMAL_PLACES, alignment);
 	show(time);
 }
 
-// Save to buffer and show a char value for the specified time
 void ShiftDisplay::show(char value, unsigned long time, char alignment) {
 	set(value, alignment);
 	show(time);
 }
 
-// Save to buffer and show a char array value for the specified time
 void ShiftDisplay::show(const char value[], unsigned long time, char alignment) {
 	set(value, alignment);
 	show(time);
 }
 
-// Save to buffer and show an Arduino String value for the specified time
 void ShiftDisplay::show(const String &value, unsigned long time, char alignment) {
 	set(value, alignment);
 	show(time);
 }
 
-// Save to buffer and show a formatted segments array for the specified time
 void ShiftDisplay::show(const byte codes[], unsigned long time) {
 	set(codes);
 	show(time);
 }
 
-// Save to buffer and show a formatted characters array and a dots array for the specified time
 void ShiftDisplay::show(const char characters[], bool dots[], unsigned long time) {
 	set(characters, dots);
 	show(time);
 }
 
-// Duplicates to retain compatibility with old versions
+// duplicates to retain compatibility with old versions
 void ShiftDisplay::insertPoint(int index) { insertDot(index); }
 void ShiftDisplay::removePoint(int index) { removeDot(index); }
 void ShiftDisplay::print(long time, int value, char alignment) { show(value, time, alignment); }
--- src/ShiftDisplay.h
+++ src/ShiftDisplay.h
@@ -21,10 +21,12 @@ const int DEFAULT_DECIMAL_PLACES = 2;
 const char DEFAULT_ALIGN_TEXT = ALIGN_LEFT;
 const char DEFAULT_ALIGN_NUMBER = ALIGN_RIGHT;
 const int MAX_DISPLAY_SIZE = 8;
+const int POV = 1; // milliseconds showing each character when iterating
 
 class ShiftDisplay {
 
 	private:
+
 		int _latchPin;
 		int _clockPin;
 		int _dataPin;
@@ -35,54 +37,57 @@ class ShiftDisplay {
 		int _displayStarts[MAX_DISPLAY_SIZE];
 		byte _buffer[MAX_DISPLAY_SIZE]; // value encoded to print
 
-		void constructSingle(int latchPin, int clockPin, int dataPin, int displayType, int displaySize);
-		void constructMultiple(int latchPin, int clockPin, int dataPin, int displayType, int displayQuantity, int displaySizes[]);
-		int countCharacters(long number);
-		int countCharacters(double number);
-		void getCharacters(long input, int size, char output[]);
-		int formatCharacters(int inSize, const char input[], int outSize, char output[], char alignment, int decimalPlaces);
-		void encodeCharacters(int size, const char input[], byte output[], int dotIndex);
-		void setBufferDot(int index, bool show);
-		void setBuffer(int start, int size, byte input[]);
-		void clearDisplay();
-		void printDisplay();
+		void constructSingle(int latchPin, int clockPin, int dataPin, int displayType, int displaySize); // Function with common instructions to be called by single display constructors
+		void constructMultiple(int latchPin, int clockPin, int dataPin, int displayType, int displayQuantity, int displaySizes[]); // Function with common instructions to be called by multiple display constructors
+
+		void showDisplay(); // Iterate buffer value on display, achieving persistence of vision
+		void clearDisplay(); // Clear display
+		void setBuffer(int start, int size, byte input[]); // Save data to buffer, ready to be read by display
+		void setBufferDot(int index, bool show); // Modify buffer, add or remove a dot at index
+		void encodeCharacters(int size, const char input[], byte output[], int dotIndex); // Encode array of characters to array of bytes read by the display
+		int formatCharacters(int inSize, const char input[], int outSize, char output[], char alignment, int decimalPlaces); // Arrange characters for display format over specified alignment, Return dot index on display or NULL if none
+		void getCharacters(long input, int size, char output[]); // Convert an integer number to an array of characters
+		int countCharacters(long number); // Calculate the length of an array of chars for an integer number
+		int countCharacters(double number); // Calculate the length of an array of chars for the integer part on a real number
 
 	public:
-		ShiftDisplay(int displayType, int displaySize);
-		ShiftDisplay(int latchPin, int clockPin, int dataPin, int displayType, int displaySize);
-		ShiftDisplay(int displayType, int displayQuantity, int displaySizes[]);
-		ShiftDisplay(int latchPin, int clockPin, int dataPin, int displayType, int displayQuantity, int displaySizes[]);
-		void set(int value, char alignment = DEFAULT_ALIGN_NUMBER);
-		void set(long value, char alignment = DEFAULT_ALIGN_NUMBER);
-		void set(double value, int decimalPlaces = DEFAULT_DECIMAL_PLACES, char alignment = DEFAULT_ALIGN_NUMBER);
-		void set(double value, char alignment); // for overriding decimalPlaces obligation in above function
-		void set(char value, char alignment = DEFAULT_ALIGN_TEXT);
-		void set(const char value[], char alignment = DEFAULT_ALIGN_TEXT);
-		void set(const String &value, char alignment = DEFAULT_ALIGN_TEXT);
-		void set(const byte codes[]);
-		void set(const char characters[], bool dots[]);
-		void setAt(int displayId, int value, char alignment = DEFAULT_ALIGN_NUMBER);
-		void setAt(int displayId, long value, char alignment = DEFAULT_ALIGN_NUMBER);
-		void setAt(int displayId, double value, int decimalPlaces = DEFAULT_DECIMAL_PLACES, char alignment = DEFAULT_ALIGN_NUMBER);
-		void setAt(int displayId, double value, char alignment);
-		void setAt(int displayId, char value, char alignment = DEFAULT_ALIGN_TEXT);
-		void setAt(int displayId, const char value[], char alignment = DEFAULT_ALIGN_TEXT);
-		void setAt(int displayId, const String &value, char alignment = DEFAULT_ALIGN_TEXT);
-		void setAt(int displayId, const byte codes[]);
-		void setAt(int displayId, const char characters[], bool dots[]);
-		void insertDot(int index);
-		void removeDot(int index);
-		void show();
-		void show(unsigned long time);
-		void show(int value, unsigned long time, char alignment = DEFAULT_ALIGN_NUMBER);
-		void show(long value, unsigned long time, char alignment = DEFAULT_ALIGN_NUMBER);
-		void show(double value, unsigned long time, int decimalPlaces = DEFAULT_DECIMAL_PLACES, char alignment = DEFAULT_ALIGN_NUMBER);
-		void show(double value, unsigned long time, char alignment);
-		void show(char value, unsigned long time, char alignment = DEFAULT_ALIGN_TEXT);
-		void show(const char value[], unsigned long time, char alignment = DEFAULT_ALIGN_TEXT);
-		void show(const String &value, unsigned long time, char alignment = DEFAULT_ALIGN_TEXT);
-		void show(const byte codes[], unsigned long time);
-		void show(const char characters[], bool dots[], unsigned long time);
+
+		ShiftDisplay(int displayType, int displaySize); // Initializes the library with the single display information and use default pin numbers
+		ShiftDisplay(int latchPin, int clockPin, int dataPin, int displayType, int displaySize); // Initializes the library with the single display information and pin numbers
+		ShiftDisplay(int displayType, int displayQuantity, int displaySizes[]); // Initializes the library with the multiple displays information and use default pin numbers
+		ShiftDisplay(int latchPin, int clockPin, int dataPin, int displayType, int displayQuantity, int displaySizes[]); // Initializes the library with the multiple displays information and pin numbers
+
+		void set(int value, char alignment = DEFAULT_ALIGN_NUMBER); // Save to buffer an int value
+		void set(long value, char alignment = DEFAULT_ALIGN_NUMBER); // Save to buffer a long value
+		void set(double value, int decimalPlaces = DEFAULT_DECIMAL_PLACES, char alignment = DEFAULT_ALIGN_NUMBER); // Save to buffer a double value
+		void set(double value, char alignment); // Save to buffer a double value with default number of decimal places //  for overriding decimalPlaces obligation in above function
+		void set(char value, char alignment = DEFAULT_ALIGN_TEXT); // Save to buffer a single char value
+		void set(const char value[], char alignment = DEFAULT_ALIGN_TEXT); // Save to buffer a char array value
+		void set(const String &value, char alignment = DEFAULT_ALIGN_TEXT); // Save to buffer an Arduino String value, manual processing for better support between Arduino cores
+		void set(const byte codes[]); // Save to buffer a formatted segments array
+		void set(const char characters[], bool dots[]); // Save to buffer a formatted characters array and a dots array
+		void setAt(int displayId, int value, char alignment = DEFAULT_ALIGN_NUMBER); // Save to a section of buffer an int value
+		void setAt(int displayId, long value, char alignment = DEFAULT_ALIGN_NUMBER); // Save to a section of buffer a long value
+		void setAt(int displayId, double value, int decimalPlaces = DEFAULT_DECIMAL_PLACES, char alignment = DEFAULT_ALIGN_NUMBER); //
+		void setAt(int displayId, double value, char alignment); //
+		void setAt(int displayId, char value, char alignment = DEFAULT_ALIGN_TEXT); //
+		void setAt(int displayId, const char value[], char alignment = DEFAULT_ALIGN_TEXT); //
+		void setAt(int displayId, const String &value, char alignment = DEFAULT_ALIGN_TEXT); //
+		void setAt(int displayId, const byte codes[]); //
+		void setAt(int displayId, const char characters[], bool dots[]); //
+		void insertDot(int index); // Modify buffer, insert dot at index
+		void removeDot(int index); // Modify buffer, remove dot at index
+		void show(); // Show buffer value for one iteration
+		void show(unsigned long time); // Show buffer value for the specified time
+		void show(int value, unsigned long time, char alignment = DEFAULT_ALIGN_NUMBER); // Save to buffer and show an int value for the specified time
+		void show(long value, unsigned long time, char alignment = DEFAULT_ALIGN_NUMBER); // Save to buffer and show a long value for the specified time
+		void show(double value, unsigned long time, int decimalPlaces = DEFAULT_DECIMAL_PLACES, char alignment = DEFAULT_ALIGN_NUMBER); // Save to buffer and show a double value for the specified time
+		void show(double value, unsigned long time, char alignment); // Save to buffer and show a double value with default number of decimal places for the specified time
+		void show(char value, unsigned long time, char alignment = DEFAULT_ALIGN_TEXT); // Save to buffer and show a char value for the specified time
+		void show(const char value[], unsigned long time, char alignment = DEFAULT_ALIGN_TEXT); // Save to buffer and show a char array value for the specified time
+		void show(const String &value, unsigned long time, char alignment = DEFAULT_ALIGN_TEXT); // Save to buffer and show an Arduino String value for the specified time
+		void show(const byte codes[], unsigned long time); // Save to buffer and show a formatted segments array for the specified time
+		void show(const char characters[], bool dots[], unsigned long time); // Save to buffer and show a formatted characters array and a dots array for the specified time
 
 	// compatibility:
 		void insertPoint(int index); // insertDot

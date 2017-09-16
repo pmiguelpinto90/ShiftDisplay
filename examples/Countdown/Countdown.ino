07c4fb4daea87dd0a044b9cd66b315bb095d870e
MiguelPynto <miguelpynto@outlook.com>
2017-09-16 23:46:49 +0100

    Example according to README

--- examples/Countdown/Countdown.ino
+++ examples/Countdown/Countdown.ino
@@ -7,19 +7,17 @@ https://miguelpynto.github.io/ShiftDisplay/
 
 #include <ShiftDisplay.h>
 
-// Arduino pin 6 connected to shift register latch, pin 7 to clock and pin 5 to data
-// common cathode display with 3 digits
-ShiftDisplay display(6, 7, 5, COMMON_CATHODE, 3);
+ShiftDisplay display(COMMON_CATHODE, 3);
 
 void setup() {
 	for (int i = 3; i > 0; i--) {
-		display.show(i, 400, ALIGN_CENTER); // save number and show it for 400ms
-		display.setDot(1, true); // add dot to saved number
+		display.show(i, 400, ALIGN_CENTER); // store number and show it for 400ms
+		display.setDot(1, true); // add dot to stored number
 		display.show(400); // show number with dot for 400ms
 	}
-	display.set("GO"); // save "GO" to buffer
+	display.set("GO"); // store "GO"
 }
 
 void loop() {
-	display.show(); // show "GO" while in loop
+	display.show(); // show stored "GO" while in loop
 }
